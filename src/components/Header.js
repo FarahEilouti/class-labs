8f4db63e55da57d9bf89ab96378396874c73b264
Farah Riad Eilouti <fara7.eilouti@gmail.com>
2022-08-25 17:16:21 +0300

    lab02 fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import React from 'react';
 class Header extends React.Component{
     render(){
         return(
-            <div>
+            <div id="Header">
                 HornedBeast
             </div>
         )
