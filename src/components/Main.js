8d661c5607d7ac4cfef56005e263d7828a1413ab
Farah Riad Eilouti <fara7.eilouti@gmail.com>
2022-08-27 17:54:36 +0300

    lab03 done

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
-import data from './data.json';
+// import data from './data.json';
 import './style.css';
-let HornedB = data.map(element => <HornedBeast id = {element._id} title = {element.title} img = {element.image_url} description = {element.description}/>)
+// let HornedB 
 
 class Main extends React.Component{
     render(){  
         return(
-
-            <div id='allCards'>
-               {HornedB}
+            
+            <div id= 'allCards'>
+               {this.props.beastData.map(element =>
+                <HornedBeast
+                id = {element._id} 
+                title = {element.title} 
+                img = {element.image_url} 
+                description = {element.description} 
+                myFunction = {this.props.myFunction}/>)
+}         
             </div>
         )
     }
